fece03bb8885b08c8adf3363754de1c7ae67179d
Perryvon <Perry.j.vr@gmail.com>
2021-08-14 15:57:37 -0700

    pushing starter website for feedback

--- src/components/cards/Demo.js
+++ src/components/cards/Demo.js
@@ -1,18 +1,49 @@
-import react, {useState} from "react";
+import {useState} from "react";
 // import { Button } from "reactstrap";
+import { motion } from "framer-motion";
 import "./cardstyles.css"
 import {Namerr} from "namerr"
 
 export const Demo = () => {
   const[data, setData] = useState(Namerr.names(30).join(", "))
 
-  // ["#f79226", "#c54536", "#3e415c"]
-  
+
+  function populateUsers(){
+    let users = Namerr.users(10)
+    let userStrings = Object.values(users).map(i => (
+      `{ fullName: '${i.fullName}',
+         email: '${i.email}', 
+         username: '${i.username}',
+        password: '${i.password.slice(0,8)}' } `) 
+    )
+    setData(`[${userStrings}]`);
+  }
   return (
     <div className="demoDiv">
-      <button className="btn databtn btn-lg" style={{border:"3px solid #f79226"}}>names</button>{' '}
-      <button className="btn databtn btn-lg" style={{border:"3px solid #c54536"}}>emails</button>{' '}
-      <button className="btn databtn btn-lg" style={{border:"3px solid #3e415c"}}>users</button>{' '}
+      <motion.button 
+          whileHover={{scale:1.1}}
+          whileTap={{scale:0.9}} 
+          className="btn databtn btn-lg" 
+          style={{border:"3px solid #f79226"}}
+          onClick={() => setData(`'${Namerr.names(80).join("', '")}'`)}
+          >names
+      </motion.button>{' '}
+      <motion.button 
+          whileHover={{scale:1.1}}
+          whileTap={{scale:0.9}} 
+          className="btn databtn btn-lg" 
+          style={{border:"3px solid #c54536"}}
+          onClick={() => setData(`'${Namerr.emails(50).join("', '")}'`)}
+          >emails
+      </motion.button>{' '}
+      <motion.button 
+          whileHover={{scale:1.1}}
+          whileTap={{scale:0.9}} 
+          className="btn databtn btn-lg" 
+          style={{border:"3px solid #3e415c"}}
+          onClick={() => setData(populateUsers)}
+          >users
+      </motion.button>{' '}
       
       <div className="demoData">
         <h5 className="dataText">{data}</h5>
--- src/components/text/Intro.js
+++ src/components/text/Intro.js
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import AnimatedCharacters from "./AnimatedCharacters";
 import "./styles.css";
 
 export default function Intro() {
-  const [replay, setReplay] = useState(true);
   // Placeholder text data, as if from API
   const placeholderText = [
     { type: "heading1", text: "Namerr is a package for generating large ammounts of \n names, emails and more on the fly!" },
@@ -29,7 +28,7 @@ export default function Intro() {
       className="App"
       initial="hidden"
       // animate="visible"
-      animate={replay ? "visible" : "hidden"}
+      // animate={replay ? "visible" : "hidden"}
       variants={container}
     >
       <div className="container">
