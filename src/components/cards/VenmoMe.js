2fe91a19fc56df6033d228ddedb1c4da33543879
Perryvon <Perry.j.vr@gmail.com>
2021-08-15 12:47:45 -0700

    almost done with base site

--- src/components/cards/VenmoMe.js
+++ src/components/cards/VenmoMe.js
@@ -5,6 +5,10 @@ import qr from "./venQR.png"
 
 export const VenmoMe = () => {
   <h3 className="support-text">Like what you see? Saved you some time?<br></br> <u>🍻  buy me a beer 🍻</u></h3>
-  return <img src={qr} class="img-fluid" alt="Responsive image"/>
+  return (
+  <div>
+    <h3 className="support-text">Like what you see? Saved you some time?<br></br> <u>🍻  buy me a beer 🍻</u></h3>
+    <img src={qr} class="img-fluid" alt="Responsive image"/>
+  </div>)
 
 };
